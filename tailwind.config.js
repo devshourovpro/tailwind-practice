ca280d103be693d75c7e549bd90b66eaa7fea9d6
devshourovpro <devshourovpro@gmail.com>
2023-02-08 22:08:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily:{
+          Poppins:"Poppins"
+        },
+        colors:{
+          dark_purple:"#000235",
+          btn_primary:"7e7ee3",
+          heading_color:"#abd9d9"
+        }
+      },
+    },
+    plugins: [],
+  }
